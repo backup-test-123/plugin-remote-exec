a793bc01869721d0795f5de0ceb38ab546e5f146
Haytham Abuelfutuh <haytham@afutuh.com>
2019-11-24 00:08:56 -0800

    Initial commit

--- flytepropeller/cmd/main.go
+++ flytepropeller/cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/flyteorg/plugin-remote-exec/go/serviceplugin"
+	_ "../../go/serviceplugin"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/container"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/sidecar"
 	"github.com/lyft/flytepropeller/cmd/controller/cmd"
